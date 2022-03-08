8ddffedef7455ec58bbbfc7e090b8627011fdaa2
Elias Cardoso <eliascardosodasilva@hotmail.com>
2022-03-08 12:23:13 -0300

    DoWhile

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int numero = 0;
+
+        while (numero <= 10) {
+            System.out.println("O numero atual " + numero);
+            numero++;
+
+// do While
+
+            //do {
+            //   System.out.println("O numero atual" + numero);
+            // numero++ }
+            //while (numero <= 10);
+
+        }
+    }
+}
